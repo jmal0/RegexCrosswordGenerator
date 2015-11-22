11eba8484a7cbc9c416a436f8e07605035b38c75
John Maloney <jmal020@gmail.com>
2015-11-21 22:43:28 -0500

    Added check puzzle function

--- RegexCrossword.py
+++ RegexCrossword.py
@@ -1,9 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+
+from CrosswordSquare import CrosswordSquare
 
 class RegexCrossword:
+    ''' Object representing the puzzle
+    The puzzle has x and y dimensions and a corresponding number of regex rules
+    '''
     def __init__(self, xSize, ySize, columnRules, rowRules):
         self.xSize = xSize
         self.ySize = ySize
-        self.grid = [[" " for i in xrange(xSize)] for i in xrange(ySize)]
+        self.grid = [[" " for c in xrange(xSize)] for r in xrange(ySize)]
         
         if(xSize != len(columnRules)):
             raise ValueError("Length of columnRules must equal xSize")
@@ -12,16 +20,32 @@ class RegexCrossword:
 
         self.cRules = columnRules
         self.rRules = rowRules
-        
+   
+    def check(self):
+        ''''Determine if filled grid satisfies all row and column rules'''
+        # Check row patterns
+        for r in xrange(self.ySize):
+            row = ''.join(self.grid[r])
+            if(not self.rRules[r].match(row)):
+                return False
+
+        # Check column patterns
+        for c in xrange(self.xSize):
+            col = ''.join([self.grid[r][c] for r in xrange(self.ySize)])
+            if(not self.cRules[c].match(col)):
+                return False
+
+        return True
+
     def __str__(self):
         puzzle = ""
         puzzle += "%-10s" % ""
         for r in xrange(self.xSize):
-            puzzle += "%-10s" % self.cRules[r]
+            puzzle += "%-10s" % self.cRules[r].pattern
         puzzle += "\n"
 
         for r in xrange(self.xSize):
-            puzzle += "%-10s" % self.rRules[r]
+            puzzle += "%-10s" % self.rRules[r].pattern
             for c in xrange(self.ySize):
                 puzzle += "%-10s" % self.grid[r][c]
             puzzle += "\n"
